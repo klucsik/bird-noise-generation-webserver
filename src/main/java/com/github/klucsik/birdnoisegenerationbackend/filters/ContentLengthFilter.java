431b2c7e043b40acb83cc35592a73ca2c05a7ae1
klucsik <krisztian.klucsik@pontsystems.eu>
2021-04-07 18:53:10 +0200

    sneding content length instead of chunked

---
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/filters/ContentLengthFilter.java
@@ -0,0 +1,37 @@
+package com.github.klucsik.birdnoisegenerationbackend.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+public class ContentLengthFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper((HttpServletResponse) response);
+
+        chain.doFilter(request, responseWrapper);
+
+        responseWrapper.copyBodyToResponse();
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
