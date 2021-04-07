fadc31f1e7e43b25b64f373e69d342526f2fe562
klucsik <krisztian.klucsik@pontsystems.eu>
2021-04-07 17:39:39 +0200

    allow cross origion request on dummy controller

--- src/main/java/com/github/klucsik/birdnoisegenerationbackend/controller/DeviceOverViewController.java
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/controller/DeviceOverViewController.java
@@ -4,6 +4,7 @@ import com.github.klucsik.birdnoisegenerationbackend.dto.BatteryDto;
 import com.github.klucsik.birdnoisegenerationbackend.dto.DeviceOverviewDto;
 import com.github.klucsik.birdnoisegenerationbackend.persistence.enums.DeviceStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("deviceOverView")
+@CrossOrigin
 public class DeviceOverViewController {
 
     //this will be accessible at baseurl/devices/page
