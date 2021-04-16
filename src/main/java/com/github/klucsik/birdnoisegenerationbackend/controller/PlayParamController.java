39d7be86ccf979caa54887b88075225741eda611
klucsik <krisztian.klucsik@pontsystems.eu>
2021-04-16 10:18:45 +0200

    playParam slice part 2 - list and delete

--- src/main/java/com/github/klucsik/birdnoisegenerationbackend/controller/PlayParamController.java
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/controller/PlayParamController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/playParam")
 @RequiredArgsConstructor
@@ -14,17 +16,29 @@ public class PlayParamController {
     private final PlayParamService service;
 
     @PostMapping("/save")
-    public ResponseEntity<PlayParamDto> saveTrack(@RequestBody PlayParamDto dto){
+    public ResponseEntity<PlayParamDto> saveTrack(@RequestBody PlayParamDto dto) {
         return new ResponseEntity<>(service.save(dto), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PlayParamDto> getOne(@PathVariable Long id) {
-        return new ResponseEntity<>(service.getOne(id),HttpStatus.OK);
+        return new ResponseEntity<>(service.getOne(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<List<PlayParamDto>> getPage() {
+        return new ResponseEntity<>(service.getAll(), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<PlayParamDto> deleteOne(@PathVariable Long id) {
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/mock")
     public ResponseEntity<PlayParamDto> getMock() {
-        return new ResponseEntity<>(service.getMock(),HttpStatus.OK);
+        return new ResponseEntity<>(service.getMock(), HttpStatus.OK);
     }
 }
