c17b3ab597153d8e48f107222b79475776c5ff7d
klucsik <krisztian.klucsik@pontsystems.eu>
2021-07-04 22:30:21 +0200

    Add validation in controller

--- src/main/java/com/github/klucsik/birdnoisegenerationbackend/dto/TrackDto.java
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/dto/TrackDto.java
@@ -2,13 +2,30 @@ package com.github.klucsik.birdnoisegenerationbackend.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Represents the Track entity for the communication with the frontend.
+ */
 @NoArgsConstructor
 @Data
 public class TrackDto {
     private Long id;
+
+    @NotNull(message = "Tracknumber is mandatory") //Validations always should be the same as on Entity
+    @Range(min = 1, max = 255)
     private Integer trackNumber;
+
+    @Size(max=255)
+    @NotBlank(message = "Name is mandatory")
     private String name;
+
+    @Range(min = 1,max = 1800)
+    @NotNull(message = "Length is mandatory")
     private Integer length;
 
    public TrackDto(Integer trackNumber,String name,Integer length){
