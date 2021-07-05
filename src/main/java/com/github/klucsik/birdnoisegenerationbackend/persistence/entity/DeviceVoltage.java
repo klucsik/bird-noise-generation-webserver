ef7f5bddfb53326108b0522a267c7ec38bfeddf2
Levenet Ódor <70817607+Levente007@users.noreply.github.com>
2021-07-05 22:34:06 +0200

    Added LocalDateTime

--- src/main/java/com/github/klucsik/birdnoisegenerationbackend/persistence/entity/DeviceVoltage.java
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/persistence/entity/DeviceVoltage.java
@@ -2,23 +2,25 @@ package com.github.klucsik.birdnoisegenerationbackend.persistence.entity;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 @NoArgsConstructor
 @Data
 public class DeviceVoltage {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //db will generate this for us
     private Float voltage;
     @ManyToOne
     private Device device;
 
-    private LocalDate createdAt; //we will set this in the service layer
-    private LocalTime createdTime;
+    private LocalDateTime createdAt = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private String formated = createdAt.format(formatter);
 
 }
