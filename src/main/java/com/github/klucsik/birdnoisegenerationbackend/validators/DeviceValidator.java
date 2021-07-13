51649b59879901ea701b76e9d3eebd18d7ef8dbb
Levenet Ódor <70817607+Levente007@users.noreply.github.com>
2021-07-13 11:39:02 +0200

    also added DeviceValidator

--- src/main/java/com/github/klucsik/birdnoisegenerationbackend/validators/DeviceValidator.java
+++ src/main/java/com/github/klucsik/birdnoisegenerationbackend/validators/DeviceValidator.java
@@ -1,5 +1,31 @@
 package com.github.klucsik.birdnoisegenerationbackend.validators;
 
-public class DeviceValidator {
+import com.github.klucsik.birdnoisegenerationbackend.persistence.entity.Device;
+import com.github.klucsik.birdnoisegenerationbackend.repository.DeviceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class DeviceValidator {
+    private final BaseValidator baseValidator;
+    private final DeviceRepository repository;
+
+    public void validate(Device device) throws MethodArgumentNotValidException {
+        List<FieldError> errors = new ArrayList<>();
+
+        if (repository.existsByChipId(device.getChipId())) {
+            errors.add(new FieldError("Device", "chipId", "ChipId must be unique"));
+        }
+
+        if (repository.existsByName(device.getName())) {
+            errors.add(new FieldError("Device", "name", "Name must be unique"));
+        }
+        baseValidator.validateAnnotations(device, errors, "Device");
+    }
 }
