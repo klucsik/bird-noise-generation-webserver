db96e979ed988b3a8c870daae856f613b5327a71
Levenet Ódor <70817607+Levente007@users.noreply.github.com>
2021-07-14 17:58:40 +0200

    Fixed Device update validation

--- backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/validators/DeviceValidator.java
+++ backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/validators/DeviceValidator.java
@@ -19,11 +19,11 @@ public class DeviceValidator {
     public void validate(Device device) throws MethodArgumentNotValidException {
         List<FieldError> errors = new ArrayList<>();
 
-        if (repository.existsByChipId(device.getChipId())) {
-            errors.add(new FieldError("Device", "chipId", "ChipId must be unique"));
+        if (repository.existsByChipId(device.getChipId()) & device.getId() == null) {
+            errors.add(new FieldError("Devcie", "chipId", "ChipId must be unique"));
         }
 
-        if (repository.existsByName(device.getName())) {
+        if (repository.existsByName(device.getName()) & device.getId() == null) {
             errors.add(new FieldError("Device", "name", "Name must be unique"));
         }
 
