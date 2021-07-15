3e87cfb28e7c05ca54338a18d7a5076c4b648057
klucsik <krisztian.klucsik@pontsystems.eu>
2021-07-15 23:47:33 +0200

    image serving, and dummy 404

---
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.github.klucsik.birdnoiseserver.frontend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/static/img/**",
+                "/static/css/**",
+                "/static/js/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+}
