5d1a1be747bb09f30db672b671353f0e0c8ffe14
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2021-08-20 23:29:05 +0200

    Device updating playparams controller, service

--- backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/repository/DevicePlayParamRepository.java
+++ backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/repository/DevicePlayParamRepository.java
@@ -1,7 +1,12 @@
 package com.github.klucsik.birdnoiseserver.backendserver.repository;
 
+import com.github.klucsik.birdnoiseserver.backendserver.persistence.entity.Device;
 import com.github.klucsik.birdnoiseserver.backendserver.persistence.entity.DevicePlayParam;
+import com.github.klucsik.birdnoiseserver.backendserver.persistence.entity.PlayParam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DevicePlayParamRepository extends JpaRepository<DevicePlayParam, Long> {
+    List<DevicePlayParam> getAllByDevice(Device device);
 }
