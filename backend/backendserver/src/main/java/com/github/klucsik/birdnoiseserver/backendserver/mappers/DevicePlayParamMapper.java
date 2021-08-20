fae1c48ab9404283c8de0597242f0f71bdf91386
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2021-08-20 19:19:47 +0200

    DevicePlayParam save, getById, getPage, delete functions

--- backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/mappers/DevicePlayParamMapper.java
+++ backend/backendserver/src/main/java/com/github/klucsik/birdnoiseserver/backendserver/mappers/DevicePlayParamMapper.java
@@ -1,38 +1,17 @@
 package com.github.klucsik.birdnoiseserver.backendserver.mappers;
 
-
-import com.github.klucsik.birdnoiseserver.backendclient.dto.*;
+import com.github.klucsik.birdnoiseserver.backendclient.dto.DevicePlayParamDto;
+import com.github.klucsik.birdnoiseserver.backendclient.dto.DevicePlayParamSlimDto;
+import com.github.klucsik.birdnoiseserver.backendserver.persistence.entity.DevicePlayParam;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Mapper
 public interface DevicePlayParamMapper {
     DevicePlayParamMapper MAPPER = Mappers.getMapper(DevicePlayParamMapper.class);
 
-    @Mapping(source = "id", target = "paramVersion")
-    @Mapping(source = "playUnits", target = "playParams")
-    DevicePlayParamDto playParamDtotoDevice(PlayParamDto playParamDto);
-
-    //do this mapping by hand becouse we change from map to list, and need to bring in the hours.
-    default List<DevicePlayUnitDto> playUnitDtotoDevice(Map<Integer, PlayUnitDto> playUnits) {
-        List<DevicePlayUnitDto> devicePlayUnitDtos = new ArrayList<>();
-        playUnits.forEach((integer, playUnitDto) -> {
-            DevicePlayUnitDto devicePlayUnitDto = new DevicePlayUnitDto();
-            devicePlayUnitDto.setHour(integer);
-            devicePlayUnitDto.setMaxT(playUnitDto.getMaxPause());
-            devicePlayUnitDto.setMinT(playUnitDto.getMinPause());
-            devicePlayUnitDto.setTracks(playUnitDto.getTrackList().stream().map(TrackDto::getTrackNumber).collect(Collectors.toList()));
-            devicePlayUnitDtos.add(devicePlayUnitDto);
-        });
-
-        return devicePlayUnitDtos;
-    }
+    DevicePlayParam dtoToDevicePLayParam(DevicePlayParamDto devicePlayParamDto);
+    DevicePlayParamDto devicePlayParamToDto(DevicePlayParam devicePlayParam);
 
 
 }
