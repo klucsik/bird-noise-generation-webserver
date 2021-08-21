07733997ad65b62b27551ce5b8970b0d7fa46715
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2021-08-21 23:47:29 +0200

    new playUnit FE (needs trackList)

--- frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/PlayUnitController.java
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/PlayUnitController.java
@@ -7,9 +7,8 @@ import com.github.klucsik.birdnoiseserver.frontend.connectors.TrackConnector;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,5 +23,25 @@ public class PlayUnitController {
         List<PlayUnitDto> playUnitDtoList = connector.getPage().getBody();
         model.addAttribute("PlayUnitDtoList",playUnitDtoList);
         return "playUnit/page";
+    }
+
+    @GetMapping("/new")
+    public String newPlayUnitForm(Model model){
+
+        model.addAttribute("playUnitDto", new PlayUnitDto());
+        return "playUnit/new";
+    }
+
+    @PostMapping("/new")
+    public String save(@ModelAttribute PlayUnitDto playUnitDto, Model model, RedirectAttributes attributes) {
+        try {
+            PlayUnitDto savedDto = connector.saveTrack(playUnitDto).getBody();
+            attributes.addFlashAttribute("message", String.format(" successful save whit id %d", savedDto.getId()));
+            return "redirect:/playUnit/page";
+        } catch (Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("errorMessage", "Error:" + e.getMessage());
+            return "redirect:/playUnit/new";
+        }
     }
 }
