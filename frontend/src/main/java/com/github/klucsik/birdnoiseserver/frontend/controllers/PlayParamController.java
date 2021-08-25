2c82c5d48f6939bbaa2666d506300f4f2da1d61d
klucsik <krisztian.klucsik@pontsystems.eu>
2021-08-26 00:09:03 +0200

    playparam save

--- frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/PlayParamController.java
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/PlayParamController.java
@@ -1,20 +1,26 @@
 package com.github.klucsik.birdnoiseserver.frontend.controllers;
 
 import com.github.klucsik.birdnoiseserver.backendclient.dto.PlayParamDto;
+import com.github.klucsik.birdnoiseserver.backendclient.dto.PlayUnitDto;
 import com.github.klucsik.birdnoiseserver.frontend.connectors.PlayParamConnector;
+import com.github.klucsik.birdnoiseserver.frontend.connectors.PlayUnitConnector;
+import com.github.klucsik.birdnoiseserver.frontend.stupidDtos.FrontEndPlayParamDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/playParam")
 @RequiredArgsConstructor
 public class PlayParamController {
     private final PlayParamConnector connector;
+    private final PlayUnitConnector playUnitConnector;
 
     @GetMapping("/page")
     public String getPage(Model model) {
@@ -25,33 +31,51 @@ public class PlayParamController {
 
     @GetMapping("/new")
     public String newPlayParamForm(Model model){
-
-        model.addAttribute("playParamDto", new PlayParamDto());
+        model.addAttribute("playUnitList", playUnitConnector.getPage().getBody());
+        PlayParamDto playParamDto =new PlayParamDto();
+        Map<Integer, PlayUnitDto> playUnits = new HashMap<>();
+        for (int i = 1; i < 24; i++) {
+            playUnits.put(i,new PlayUnitDto());
+        }
+        playParamDto.setPlayUnits(playUnits);
+        model.addAttribute("playParamDto", playParamDto);
         model.addAttribute("title", "New PlayParam");
         return "playParam/save";
     }
 
     @GetMapping("/{id}")
     public String editPlayParamFrom(@PathVariable Long id, Model model){
-
+        model.addAttribute("playUnitList", playUnitConnector.getPage().getBody());
         model.addAttribute("playParamDto", connector.getOne(id).getBody());
         model.addAttribute("title", "Edit title");
         return "playParam/save";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute PlayParamDto playParamDto,Model model, RedirectAttributes attributes) {
+    public String save(@ModelAttribute FrontEndPlayParamDto frontEndDto, Model model, RedirectAttributes attributes) {
         try {
-            PlayParamDto savedDto = connector.saveTrack(playParamDto).getBody();
+            PlayParamDto mappedDto = new PlayParamDto();
+            mappedDto.setId(frontEndDto.getId());
+            mappedDto.setName(frontEndDto.getName());
+            mappedDto.setVol(frontEndDto.getVol());
+            Map<Integer, PlayUnitDto> playUnits = new HashMap<>();
+            frontEndDto.getPlayUnits().forEach((hour, playUnitId) -> {
+                if (playUnitId != null){
+                    playUnits.put(hour,playUnitConnector.getOne(playUnitId).getBody());
+                }
+            });
+            mappedDto.setPlayUnits(playUnits);
+            PlayParamDto savedDto = connector.save(mappedDto).getBody();
             attributes.addFlashAttribute("message", String.format(" successful save with id %d",savedDto.getId()));
             return "redirect:/playParam/page";
         } catch (Exception e){
             e.printStackTrace();
             attributes.addFlashAttribute("errorMessage", "Error: " + e.getMessage());
-            if(playParamDto.getId() != null){
-                return String.format("redirect:/track/%d",playParamDto.getId());
+            if(frontEndDto.getId() != null){
+                return String.format("redirect:/track/%d",frontEndDto.getId());
             }
-            return "redirect:/playParam/save";
+            //TODO:send back the data to fill out the page
+            return "redirect:/playParam/new";
         }
     }
 }
