4b9532cd101098525a0d2a4f8cd45b986760f628
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2021-08-28 21:04:51 +0200

    DevicePlayParam delete

--- frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/DevicePlayParamController.java
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/DevicePlayParamController.java
@@ -2,7 +2,6 @@ package com.github.klucsik.birdnoiseserver.frontend.controllers;
 
 import com.github.klucsik.birdnoiseserver.backendclient.dto.DeviceDto;
 import com.github.klucsik.birdnoiseserver.backendclient.dto.DevicePlayParamDto;
-import com.github.klucsik.birdnoiseserver.backendclient.dto.TrackDto;
 import com.github.klucsik.birdnoiseserver.frontend.connectors.DeviceConnector;
 import com.github.klucsik.birdnoiseserver.frontend.connectors.DevicePlayParamConnector;
 import com.github.klucsik.birdnoiseserver.frontend.connectors.PlayParamConnector;
@@ -25,34 +24,34 @@ public class DevicePlayParamController {
     private final PlayParamConnector playParamConnector;
 
     @GetMapping("/page")
-    public String getAllById(Model model,@RequestParam Long id) {
+    public String getAllById(Model model, @RequestParam Long id) {
         List<DevicePlayParamDto> devicePlayParamDtos = connector.getAllByDevice(id).getBody();
         model.addAttribute("devicePlayParamDtos", devicePlayParamDtos);
         model.addAttribute("deviceId", id);
-        return "DevicePlayParam/page";
+        return "devicePlayParam/page";
     }
 
     @GetMapping("/new")
-    public String newDevicePlayParamForm(@RequestParam Long id, Model model){
+    public String newDevicePlayParamForm(@RequestParam Long id, Model model) {
         DeviceDto deviceDto = deviceConnector.getById(id).getBody();
         model.addAttribute("playParamList", playParamConnector.getPage().getBody());
         DevicePlayParamDto devicePlayParamDto = new DevicePlayParamDto();
         devicePlayParamDto.setDevice(deviceDto);
 
         model.addAttribute("devicePlayParamDto", devicePlayParamDto);
-        model.addAttribute("title","New devicePlayParam");
+        model.addAttribute("title", "New devicePlayParam");
         return "/devicePlayParam/save";
     }
 
     @GetMapping("/{id}")
-    public String editDevicePlayParamForm(@PathVariable Long id, Model model){
+    public String editDevicePlayParamForm(@PathVariable Long id, Model model) {
         model.addAttribute("devicePlayParamDto", connector.getById(id).getBody()); //TODO make getOnes consistent pls
-        model.addAttribute("title","Edit devicePlayParam");
+        model.addAttribute("title", "Edit devicePlayParam");
         return "/devicePlayParam/save";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute FrontEndDevicePlayParamDto frontEndDevicePlayParamDto, RedirectAttributes attributes){
+    public String save(@ModelAttribute FrontEndDevicePlayParamDto frontEndDevicePlayParamDto, RedirectAttributes attributes) {
         try {
             DevicePlayParamDto mappedDto = new DevicePlayParamDto();
             mappedDto.setStartTime(LocalDateTime.parse(frontEndDevicePlayParamDto.getStartTime()));
@@ -60,16 +59,30 @@ public class DevicePlayParamController {
             mappedDto.setDevice(deviceConnector.getById(frontEndDevicePlayParamDto.getDevice()).getBody());
             mappedDto.setPlayParam(playParamConnector.getOne(frontEndDevicePlayParamDto.getPlayParam()).getBody());
             DevicePlayParamDto savedDto = connector.save(mappedDto).getBody();
-            attributes.addFlashAttribute("message", String.format("Successful save with id %d",savedDto.getId()));
+            attributes.addFlashAttribute("message", String.format("Successful save with id %d", savedDto.getId()));
             return String.format("redirect:/devicePlayParam/page?id=%d", savedDto.getDevice().getId());
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             attributes.addFlashAttribute("errorMessage", "Error: " + e.getMessage());
 
-            if(frontEndDevicePlayParamDto.getId() != null){
+            if (frontEndDevicePlayParamDto.getId() != null) {
                 return String.format("redirect:/devicePlayParam/%d", frontEndDevicePlayParamDto.getId());
             }
             return String.format("redirect:/devicePlayParam/new?id=%d", frontEndDevicePlayParamDto.getDevice());
+        }
+    }
+
+    @GetMapping("/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
+        DevicePlayParamDto devicePlayParamDto = connector.getById(id).getBody();
+        try {
+            connector.delete(id);
+            attributes.addFlashAttribute("message",("Successful delete"));
+            return String.format("redirect:/devicePlayParam/page?id=%d", devicePlayParamDto.getDevice().getId());
+        }catch (Exception e) {
+            e.printStackTrace();
+            attributes.addFlashAttribute(e.getMessage());
+            return String.format("redirect:/devicePlayParam/page?id=%d", devicePlayParamDto.getDevice().getId());
         }
     }
 }
