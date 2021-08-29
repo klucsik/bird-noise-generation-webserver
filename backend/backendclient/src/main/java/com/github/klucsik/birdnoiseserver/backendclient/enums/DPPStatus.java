0bf59042658d5b2f6cf01a3c98669c8fbd1aa916
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2021-08-29 17:03:41 +0200

    Added Status Machine BE

---
+++ backend/backendclient/src/main/java/com/github/klucsik/birdnoiseserver/backendclient/enums/DPPStatus.java
@@ -0,0 +1,10 @@
+package com.github.klucsik.birdnoiseserver.backendclient.enums;
+
+public enum DPPStatus {
+    DRAFT,
+    DEPLOYABLE,
+    DEPLOYING,
+    DEPLOYED,
+    UNDEPLOYING,
+    DELETED
+}
