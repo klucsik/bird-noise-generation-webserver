fd081e496a4d447919c32c55a8a20ec545864645
Ódor Levente <70817607+Levente007@users.noreply.github.com>
2022-03-16 13:07:00 +0100

    Get updated devices Fe

--- frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/connectors/DeviceConnector.java
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/connectors/DeviceConnector.java
@@ -3,10 +3,7 @@ package com.github.klucsik.birdnoiseserver.frontend.connectors;
 import com.github.klucsik.birdnoiseserver.backendclient.dto.DeviceDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,9 @@ public interface DeviceConnector {
 
     @GetMapping("/freshVersion")
     ResponseEntity<String> getFreshVersion();
+
+    @GetMapping("/versionChecker")
+    ResponseEntity<Integer> versionChecker(@RequestParam String version);
 
     @DeleteMapping("/{id}")
     ResponseEntity<DeviceDto> delete(@PathVariable Long id);
