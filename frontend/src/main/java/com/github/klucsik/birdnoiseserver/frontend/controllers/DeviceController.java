f43933f0dd14954391e1b2c7f68111583a6ff0ab
klucsik <klucsik.krisztian@gmail.com>
2022-04-04 10:53:20 +0200

    howmanydays option for ErrorLogs

--- frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/DeviceController.java
+++ frontend/src/main/java/com/github/klucsik/birdnoiseserver/frontend/controllers/DeviceController.java
@@ -82,7 +82,7 @@ public class DeviceController {
     public String logs(@PathVariable Long id, Model model){
         List<DeviceLogDto> dtoList = logConnector.pageByDeviceId(id).getBody();
         List<FrontEndDeviceLogDto> stupidDtoList = mapDeviceLogDtos(dtoList);
-
+        model.addAttribute("isErrorLogsPage", false);
         model.addAttribute("logs", stupidDtoList);
         model.addAttribute("title", String.format("Logs for device %s" ,connector.getById(id).getBody().getName()));
         return "device/logPage";
@@ -92,6 +92,7 @@ public class DeviceController {
     public String editDeviceForm(@RequestParam(required = false, defaultValue = "3") Integer day, Model model){
         List<DeviceLogDto> dtoList = logConnector.pageByErrorsInDays(day).getBody();
         List<FrontEndDeviceLogDto> stupidDtoList = mapDeviceLogDtos(dtoList);
+        model.addAttribute("isErrorLogsPage", true);
         model.addAttribute("logs", stupidDtoList);
         model.addAttribute("title", String.format("Error logs in the last %d days", day));
         return "device/logPage";
